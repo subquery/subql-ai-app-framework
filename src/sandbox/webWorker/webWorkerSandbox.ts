b3851c03f8d98d4142649892842cb0bb6aaf3c04
Scott Twiname <skott.twiname@gmail.com>
2024-10-29 17:05:44 +1300

    Update import permissions

--- src/sandbox/webWorker/webWorkerSandbox.ts
+++ src/sandbox/webWorker/webWorkerSandbox.ts
@@ -42,7 +42,17 @@ const IPFS_PERMISSIONS = (dir?: string): Deno.PermissionOptionsObject => ({
 const LOCAL_PERMISSIONS: Deno.PermissionOptionsObject = {
   read: true,
   ffi: true,
-};
+  // These are the same as the default locations https://docs.deno.com/runtime/fundamentals/security/
+  import: [
+    "deno.land",
+    "jsr.io",
+    "esm.sh",
+    "raw.githubusercontent.com",
+    "gist.githubusercontent.com",
+  ],
+  // Types are missing for import but the feature is there https://github.com/denoland/deno/issues/26074
+  // deno-lint-ignore no-explicit-any
+} as any;
 
 function getPermisionsForSource(
   source: Source,
